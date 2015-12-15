e00ff78f40f192f53b3dfbce048835d4a3fac307
narrowdesign <nick@narrowdesign.com>
2015-12-14 20:33:22 -0500

    interactive

--- ui/assets/scripts/script.js
+++ ui/assets/scripts/script.js
@@ -1,15 +1,21 @@
 $(function() {
 	var WIN = $(window);
 	var DOC = $(document);
-	
+	placeHashes()
+	WIN.on('resize',placeHashes)
 	$('.slider').on('mousedown',function(e){
+		e.preventDefault();
 		var slider = $(this)
-		console.log(slider)
+		
 		WIN.on('mousemove',function(e){
 			if(slider.hasClass('you-slider')){
-				var width = e.pageX- slider.offset().left;
+				$('.you-slider').addClass("inactive");
+				var width = e.pageX - slider.offset().left;
 			}else{
+				$('.son-slider').addClass("inactive");
+				$('.window-slider').addClass('active')
 				var width = (WIN.width() - e.pageX - WIN.width()/10) - (WIN.width()-$('.you-slider').width() - (WIN.width() - $('.timeline').width()));
+				setWindowWidth();
 			}
 			slider.find($('.age')).html(Math.floor(80*(width/$('.timeline').width())));
 			slider.css({
@@ -23,9 +29,25 @@ $(function() {
 						right: WIN.width()-$('.you-slider').width() - (WIN.width() - $('.timeline').width())
 					})
 				}else{
-
+					$('.window-years').html((18-Number($('.son-slider').find($('.age')).text()))+' years.')
+					$('.window-slider').find($('.bar')).addClass('blink')
+					setWindowWidth();
 				}
 			})
 		})
 	})
+	function setWindowWidth(){
+		$('.window-slider').find($('.your-age')).html(18-Number($('.son-slider').find($('.age')).text())+Number($('.you-slider').find($('.age')).text()))
+		$('.window-slider').css({
+			width: $('.timeline').width()*((18-$('.son-slider').find($('.age')).text())/80),
+			left: $('.you-slider').width()
+		});
+	}
+	function placeHashes(){
+		for (var i = $('.hash').length - 1; i >= 0; i--) {
+			$('.hash').eq(i).css({
+				left: i*$('.timeline').width()/8
+			})
+		};
+	}
 })
