c3a29316141146cca94adaf645fe2c6202d6f282
narrowdesign <nick@narrowdesign.com>
2015-12-14 21:24:45 -0500

    touch

--- ui/assets/scripts/script.js
+++ ui/assets/scripts/script.js
@@ -1,41 +1,54 @@
+var slider;
 $(function() {
 	var WIN = $(window);
 	var DOC = $(document);
 	placeHashes()
 	WIN.on('resize',placeHashes)
-	$('.slider').on('mousedown',function(e){
+	$('.slider').on('mousedown',startDrag)
+	$('.slider').on('touchstart',startDrag)
+	function startDrag (e) {
 		e.preventDefault();
-		var slider = $(this)
-		
-		WIN.on('mousemove',function(e){
-			if(slider.hasClass('you-slider')){
-				$('.you-slider').addClass("inactive");
-				var width = e.pageX - slider.offset().left;
-			}else{
-				$('.son-slider').addClass("inactive");
-				$('.window-slider').addClass('active')
-				var width = (WIN.width() - e.pageX - WIN.width()/10) - (WIN.width()-$('.you-slider').width() - (WIN.width() - $('.timeline').width()));
-				setWindowWidth();
-			}
-			slider.find($('.age')).html(Math.max(0,Math.floor(80*(width/$('.timeline').width()))));
-			slider.css({
-				width: Math.max(5,Math.min($('.timeline').width(),width))
-			})
-			WIN.on('mouseup',function(){
-				WIN.off('mousemove')
-				if(slider.hasClass('you-slider')){
-					$('.son-slider').addClass("active");
-					$('.son-slider').css({
-						right: WIN.width()-$('.you-slider').width() - (WIN.width() - $('.timeline').width())
-					})
-				}else{
-					$('.window-years').html((18-Number($('.son-slider').find($('.age')).text()))+' years.')
-					$('.window-slider').find($('.bar')).addClass('blink');
-					setWindowWidth();
-				}
-			})
+		slider = $(this)
+		WIN.on('mousemove',onMove)
+		WIN.on('touchmove',onMove)
+	}
+	function onMove (e) {
+		var x;
+		if(e.type == 'touchmove'){
+			x = e.originalEvent.touches[0].pageX;
+		}else{
+			x = e.pageX
+		}
+		if(slider.hasClass('you-slider')){
+			$('.you-slider').addClass("inactive");
+			var width = x - slider.offset().left;
+		}else{
+			$('.son-slider').addClass("inactive");
+			$('.window-slider').addClass('active')
+			var width = (WIN.width() - x - WIN.width()/10) - (WIN.width()-$('.you-slider').width() - (WIN.width() - $('.timeline').width()));
+			setWindowWidth();
+		}
+		slider.find($('.age')).html(Math.max(0,Math.floor(80*(width/$('.timeline').width()))));
+		slider.css({
+			width: Math.max(5,Math.min($('.timeline').width(),width))
 		})
-	})
+		WIN.on('mouseup',onRelease)
+		WIN.on('touchend',onRelease)
+	}
+	function onRelease (e) {
+		WIN.off('mousemove')
+		WIN.off('touchmove')
+		if(slider.hasClass('you-slider')){
+			$('.son-slider').addClass("active");
+			$('.son-slider').css({
+				right: WIN.width()-$('.you-slider').width() - (WIN.width() - $('.timeline').width())
+			})
+		}else{
+			$('.window-years').html((18-Number($('.son-slider').find($('.age')).text()))+' years.')
+			$('.window-slider').find($('.bar')).addClass('blink');
+			setWindowWidth();
+		}
+	}
 	function setWindowWidth(){
 		$('.window-slider').find($('.your-age')).html(Math.max(Number($('.you-slider').find($('.age')).text()),18-Number($('.son-slider').find($('.age')).text())+Number($('.you-slider').find($('.age')).text())))
 		$('.window-slider').css({
